dcc0fb8fbc3b84f4d295140748d1e775036431da
Relu12345 <aurel.coruian02@e-uvt.ro>
2023-12-04 12:31:06 +0200

    asa testam putin userii

--- server/app.py
+++ server/app.py
@@ -1,19 +1,31 @@
+from datetime import datetime
 import os
+import bcrypt
+from dotenv import load_dotenv
 from flask import Flask, request, render_template, session, jsonify, send_file
 from bs4 import BeautifulSoup
+from flask_pymongo import PyMongo
 import requests, time, random
 #from api.routes import api 
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import JWTManager, create_access_token
 from json_to_pdf import generate_pdf
 from json_to_csv import generate_csv
 
 app = Flask(__name__, static_folder='static')
 #CORS disabled to be able to access the backend from the react frontend
 CORS(app)
+mongo = PyMongo(app)
+bcyrypt = bcrypt(app)
+jwt = JWTManager(app)
+
+load_dotenv()
+connection_string: str = os.environ.get('CONNECTION_STRING')
 
 #Get the secret key from the .env file
 app.config["JWT_SECRET_KEY"] = os.environ.get('FLASK_JWT')
+app.config['MONGO_DBNAME'] = 'proiect'
+app.config["MONGO_URI"] = connection_string
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
@@ -22,6 +34,72 @@ headers = {
 }
 
 session = requests.Session()
+
+@app.route("/users/register",methods=['POST'])
+def register():
+	users = mongo.db.users
+	first_name = request.get_json()['first_name']
+	last_name = request.get_json()['last_name']
+	user_name = request.get_json()['user_name']
+	email = request.get_json()['email']
+	password = bcyrypt.generate_password_hash(request.get_json()['password']).decode('utf-8')
+	created = datetime.utcnow()
+
+	user_id = users.insert({
+		'first_name': first_name,
+		'last_name': last_name,
+		'user_name': user_name,
+		'email': email,
+		'password': password,
+		'created': created,
+		})
+
+	new_user = users.find_one({'_id': user_id})
+
+	result = {'email': new_user['email']+' registered'}
+
+	return jsonify({'result':result})
+
+@app.route("/users/register",methods=['GET'])
+def get_users():
+	users = mongo.db.users
+
+	result = []
+
+	for field in users.find():
+		result.append({'_id':str(field['_id']), 'user_name':str(field['user_name']),'email':str(field['email']),'password':str(field['password'])})
+
+	return jsonify(result)
+
+
+
+
+@app.route("/users/login",methods=['POST'])
+def login():
+	users = mongo.db.users
+	email = request.get_json()['email']
+	password = request.get_json()['password']
+	result = ""
+
+	response = users.find_one({'email':email})
+
+	if response:
+		if bcyrypt.check_password_hash(response['password'], password):
+			access_token = create_access_token(identity = {
+				'first_name': response['first_name'],
+				'last_name': response['last_name'],
+				'email': response['email']
+				})
+
+			result = jsonify({"token": access_token})
+
+		else:
+			result = jsonify({"error": "Invalid Username or Password"})
+
+	else:
+		result = jsonify({"result": "No result found"})
+
+	return result
 
 @app.route('/')
 def index():
