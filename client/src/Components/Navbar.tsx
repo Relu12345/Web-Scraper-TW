3f3443c0e38e3bbbcc70426092fb43493dfe8af7
ReddSnake <horatiu.crisan01@e-uvt.ro>
2023-12-16 10:53:37 +0200

    Home page change and add to favorites functionality

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,63 +1,20 @@
-import React, {useState} from "react"
+import React from "react"
 import {BsStack, BsMoonStars} from 'react-icons/bs/'
 import {removeTokenFromCookies} from '../API/verifyToken'
-import { searchText } from "../API/searchText"
 import { useNavigate } from "react-router"
 import { Profile } from "./Profile"
 import { FaSun } from "react-icons/fa";
 
 
-
-interface ResponseMessage {
-    message: string
-    text: ResponseMessageText[]
-}
-
-interface ResponseMessageText {
-    authors: Array<string>,
-    title: string,
-    url: string
-}
-
 interface NavbarProps {
     toggleSidebar: () => void;
-    onUpdateData: (newData: ResponseMessageText[]) => void
     handleTheme: () => void
     handleLoading: (value: boolean) => void
   }
 
-let responseText: ResponseMessageText[] = []
 
 const Navbar: React.FC<NavbarProps> = (props) => {
     const navigate = useNavigate()
-    const [searchInput, setSearchInput] = useState<string>('')
-    
-    //Store the search result
-
-    const handleKeyDown = (event:  React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter') {
-            console.log('Enter key pressed');
-            handleSendData();
-            props.handleLoading(true)
-        }
-    }
-
-    const handleSendData = async () => {
-        console.log('Sending data:', searchInput);
-        const result = await searchText(searchInput)
-        
-        
-        if (result) {
-            //pass the response data to the onUpdateData callback function
-            const data: ResponseMessage = await result.json()
-            responseText = data.text
-            props.onUpdateData(responseText)
-            props.handleLoading(false)
-        }
-        else
-            console.log("Error sending text")
-    }
-
 
     const LogoutUser = () => {
         try {
@@ -76,19 +33,7 @@ const Navbar: React.FC<NavbarProps> = (props) => {
                     <span className="mr-3 mt-1 text-black dark:text-white cursor-pointer" onClick={props.toggleSidebar}>
                         <BsStack />
                     </span>
-                    Web Scraper
-                </div>
-
-                {/* Search Bar */}
-                <div className="hidden relative md:inline-block">
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        className=" md:w-4/5 lg:w-full xl:w-80 border-2 border-gray-300 bg-gray-100 h-10 px-5 pr-10 rounded-full focus:outline-none focus:border-gray-500 dark:bg-gray-400 dark:border-gray-400 focus:dark:border-white dark:placeholder-gray-800 slow-change"
-                        onChange={(event) => setSearchInput(event.target.value)}
-                        value={searchInput}
-                        onKeyDown={handleKeyDown}
-                    />
+                    FetchFlow
                 </div>
 
                 {/* Navigation Links */}
@@ -98,7 +43,7 @@ const Navbar: React.FC<NavbarProps> = (props) => {
                         onClick={props.handleTheme}
                         className="cursor-pointer w-20 h-10 -mt-1 relative rounded-full border-2 border-gray-500 bg-white dark:border-white dark:bg-slate-800">
                         <span className={` w-2/5 h-4/5 absolute rounded-full ml-2 my-1 bg-slate-800 dark:bg-white dark:ml-10 transition-all duration-500`}>
-                            {localStorage.theme === "dark" ? <FaSun className="mt-1 mx-1.5"/> : <BsMoonStars className="mt-1 mx-1.5 text-white"/>}
+                            {localStorage.theme === "dark" ? <BsMoonStars className="mt-1 mx-1.5"/> : <FaSun className="mt-1 mx-1.5 text-white"/>}
                         </span>
                    </div>
                 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,15 @@
 import React, {useState} from 'react'
-import Navbar from '../Components/Navbar'
-import Sidebar from '../Components/Sidebar'
 import Results from '../Components/Results'
+import { FaHouseChimney, FaMagnifyingGlass } from "react-icons/fa6"
+import Logo from '../Images/logo_transparent-1.svg'
+import WhiteLogo from '../Images/white-logo.png'
 import LoadingScreen from '../utils/LoadingScreen'
+import { searchText } from '../API/searchText'
+
+interface ResponseMessage {
+  message: string
+  text: ResponseMessageText[]
+}
 
 interface ResponseMessageText {
   authors: Array<string>,
@@ -10,53 +17,78 @@ interface ResponseMessageText {
   url: string
 }
 
-interface Props {
-  handleTheme: () => void
-}
 
-export const Home: React.FC<Props> = ({handleTheme}) => {
 
-    //sidebar toggle functionality
-    const [isToggleStateSidebar, setToggleStateSidebar] = useState<boolean>(true)
-    //gettin the search result from the navbar
-    const [searchData, setSearchData] = useState<ResponseMessageText[]>([])
-    const [loading, setLoading] = useState<boolean>(false)
+export const Home: React.FC = () => {
+  const [searchInput, setSearchInput] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [searchData, setSearchData] = useState<ResponseMessageText[]>([])
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      console.log('Enter key pressed')
+      handleSendData()
+    }
+  }
+
+  const handleSendData = async () => {
+    if (searchInput.length > 0) {
+      setLoading(true)
+      console.log('Sending data ', searchInput)
+      const result = await searchText(searchInput)
+
+      if (result) {
+        const data: ResponseMessage = await result.json()
+        setSearchData(data.text)
+        setLoading(false)
+      }
+    }
+  }
   
-    const toggleStateSidebar = () => {
-      setToggleStateSidebar(!isToggleStateSidebar)
-    }
-  
-    const updateSearchData = (newData: ResponseMessageText[]): void => {
-      setSearchData(newData)
-    }
-
-    const handleLoading = (value: boolean) =>{
-      setLoading(value)
-    }
-  
-    const callbackFunctions = {
-      toggleSidebar: toggleStateSidebar,
-      onUpdateData: updateSearchData,
-      handleTheme: handleTheme,
-      handleLoading: handleLoading
-    }
-
     return (
-      <div className='w-full'>
-        {/* Pass the function to the navbar component in order to change the state of the sidebar*/}
-        
-        <Navbar {...callbackFunctions}/>
-        <div className='flex dark:bg-slate-800 slow-change'>
-          <div className={`${isToggleStateSidebar? ' w-min md:w-2/6 md:w-60' : 'w-min'} flex relative bg-white shadow-md dark:bg-gray-900 slow-change`}>
-          <Sidebar isVisible={isToggleStateSidebar} />
-          </div>
-          <div className='flex-1 ml-1 lg:ml-8 dark:bg-slate-800'>
-          { 
-            loading? <LoadingScreen /> :           
+       loading ? 
+        <LoadingScreen />
+      :
+      <div className='block mt-4'>
+            <div className='flex text-xl font-bold dark:text-white'>
+              <FaHouseChimney className='mt-1'/>
+              <h1 className='mx-2'>Home</h1>
+            </div>
+
+            
+            {searchData.length === 0 &&
+            <div>
+              {
+                localStorage.theme === 'dark' ?
+                <img src={WhiteLogo} className='flex w-1/3 2xl:w-1/4 items-center justify-center mx-auto' alt="White logo" /> :
+                <img src={Logo} className='flex w-1/3 2xl:w-1/4 items-center justify-center mx-auto' alt="Logo" /> 
+              }
+              
+              <h1 className='w-1/2 text-sm lg:text-lg mx-auto text-center font-semibold dark:text-white'>Revolutionize Your Research Journey: FetchFlow - Empowering Minds, Unveiling Potential</h1>
+            </div> 
+              
+            }
+            {/* Search bar */}
+            <div className='flex mx-auto items-center justify-center text-center mt-6 relative'>
+              <input 
+                type="text"
+                id="text"
+                placeholder='Search for your favorite scientific paper...'
+                autoComplete='off'
+                value={searchInput}
+                onKeyDown={handleKeyDown}
+                onChange={(event) => setSearchInput(event.target.value)}
+                className='w-10/12 border-2 border-gray-600 bg-white py-2.5 rounded-md px-2 focus:outline-none focus:border-gray-800 dark:bg-slate-900 dark:border-gray-400 focus:dark:border-gray-200 dark:placeholder-gray-400 dark:text-white slow-change'
+              />
+              <button 
+                onClick={handleSendData}
+                className='w-1/12 top-0 end-0 h-full py-4 bg-blue-600 dark:bg-blue-700 rounded-md mx-1'
+              >
+                <FaMagnifyingGlass className='flex mx-auto text-white ' />
+              </button>
+            </div>
+
             <Results searchedData={searchData} />
-          }
-          </div>
         </div>
-      </div>
     )
 }
