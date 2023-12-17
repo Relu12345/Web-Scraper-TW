30a59d37a371f336379d386f4015a73658e1e82a
Relu12345 <aurel.coruian02@e-uvt.ro>
2023-12-17 20:45:37 +0200

    stylized pdf

--- server/json_to_pdf.py
+++ server/json_to_pdf.py
@@ -1,4 +1,4 @@
-from json2html import *
+from jinja2 import Template
 import json
 import pdfkit
 
@@ -8,7 +8,71 @@ class PdfConverter(object):
         pass
 
     def to_html(self, json_doc):
-        return json2html.convert(json=json_doc)
+        template_string = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Research Papers</title>
+            <style>
+                body {
+                    font-family: 'Arial', sans-serif;
+                    background-color: #f4f4f4;
+                    padding: 20px;
+                }
+                h1 {
+                    color: #333;
+                    text-align: center;
+                }
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                    margin-top: 20px;
+                }
+                th, td {
+                    border: 1px solid #ddd;
+                    padding: 12px;
+                    text-align: left;
+                }
+                th {
+                    background-color: #4CAF50;
+                    color: white;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Research Papers</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Authors</th>
+                        <th>URL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {% for entry in entries %}
+                        <tr>
+                            <td>{{ entry.get('title', 'No Title') }}</td>
+                            <td>
+                                {% for author in entry.get('authors', ['No Authors']) %}
+                                    {{ author }}{% if not loop.last %}, {% endif %}
+                                {% endfor %}
+                            </td>
+                            <td><a href="{{ entry.get('url', '#') }}">{{ entry.get('url', '#') }}</a></td>
+                        </tr>
+                    {% endfor %}
+                </tbody>
+            </table>
+        </body>
+        </html>
+        """
+
+        template = Template(template_string)
+        html = template.render(entries=json_doc)
+
+        return html
 
     def to_pdf(self, html_str):
         config = pdfkit.configuration(wkhtmltopdf='server\\wkhtmltopdf.exe')
@@ -23,4 +87,4 @@ def get_options():
 def generate_pdf(data):
     pdfc = PdfConverter()
     with open("server\\exports\\pdf\\Selected_papers.pdf", "wb") as pdf_fl:
-        pdf_fl.write(pdfc.to_pdf(pdfc.to_html(json.dumps(data))))
+        pdf_fl.write(pdfc.to_pdf(pdfc.to_html(data)))
